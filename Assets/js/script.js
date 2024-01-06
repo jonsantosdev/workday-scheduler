69c6f662ba74e510f0e41bb154e426586bbf8a6d
jonsantosdev <js.jsantos.jr@gmail.com>
2024-01-05 19:32:18 -0800

    modified html, js files

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,169 @@
+$(document).ready(function(){
+        $(".saveBtn").on("click", function(){
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, value);
+
+        $(".notification").addClass("show")
+
+        setTimeout(function(){
+            $(".notification").removeClass('show');
+        }, 5000);
+    });
+  
+      function hourUpdate(){
+         var currentHour = moment().hours();
+         console.log(currentHour);
+
+         $(".time-block").each(function(){
+            var blockHour = parseInt($(this).attr("id").split("-")[1]);
+            if(blockHour < currentHour){
+                $(this).addClass("past");
+            } else if(blockHour === currentHour){
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+         })
+      }
+
+      hourUpdate();
+
+      var interval = setInterval(hourUpdate, 15000)
+        
+      $("#hour-9 .description").val(localStorage.getItem("hour-9")) 
+      $("#hour-10 .description").val(localStorage.getItem("hour-10")) 
+      $("#hour-11 .description").val(localStorage.getItem("hour-11")) 
+      $("#hour-12 .description").val(localStorage.getItem("hour-12")) 
+      $("#hour-13 .description").val(localStorage.getItem("hour-13")) 
+      $("#hour-14 .description").val(localStorage.getItem("hour-14")) 
+      $("#hour-15 .description").val(localStorage.getItem("hour-15")) 
+      $("#hour-16 .description").val(localStorage.getItem("hour-16")) 
+      $("#hour-17 .description").val(localStorage.getItem("hour-17")) 
+      
+      $("#currentDay").text(dayjs().format('dddd, MMM D YYYY'));
+      //$("#currentDay").text(moment().format("dddd, MMMM Do Y"));
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+// $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +182,5 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+// });
+
